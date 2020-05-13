c5e6b6927d626728f704e831fecf789d11f8629c
Vitor Hainosz <vitorhainosz2@gmail.com>
2020-05-13 14:10:49 -0300

    usando lamba
    trocando o metodo de filtr das listas por lambdas

--- src/busines/ListagemBusines.kt
+++ src/busines/ListagemBusines.kt
@@ -8,22 +8,13 @@ import repository.ContaRepository
 class ListagemBusines {
 
     fun listarCorrentes() : List<Conta> {
-        val listaContas = mutableListOf<Conta>()
-        for (con:Conta in ContaRepository.lista()) {
-            if(con is ContaCorrente) {
-                con.limite
-                listaContas.add(con)
-            }
-        }
-        return listaContas
+        val lisarCorrentes = { ContaRepository.lista().filter { it is ContaCorrente }}
+        return lisarCorrentes()
     }
 
     fun listarPoupanca() : List<Conta> {
-        val listaContas = mutableListOf<Conta>()
-        for (con:Conta in ContaRepository.lista()) {
-            if(con is ContaPoupanca) listaContas.add(con)
-        }
-        return listaContas
+        val listarPoupancas = { ContaRepository.lista().filter { it is ContaPoupanca }}
+        return listarPoupancas()
     }
 
     fun listarContas() : List<Conta> {
